2eb13d743792877e837ffab563ed0035449f8bae
muragesh <muregeshprince@gmail.com>
2021-12-15 22:05:37 +0530

    hai

---
+++ src/arrays/multipliers.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class multipliers {
+
+	public static void main(String[] args) {
+
+	final int TOTAL_ROW = 10;
+	final int TOTAL_COL =TOTAL_ROW;
+	for(int row=1;row<TOTAL_ROW;row++) {
+	
+		System.out.printf("%3d",row);
+	}
+	System.out.println("");
+	
+	}
+  }
+
