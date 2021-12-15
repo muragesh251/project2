e866645b0114e424475b979bc786b8b0390cd9e9
muragesh <muregeshprince@gmail.com>
2021-12-15 22:00:23 +0530

    Array code

---
+++ src/arrays/StringArray.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class StringArray {
+
+	public static void main(String[] args) {
+		String student[]=new String[5];
+		
+		student[0]="Muragesh";
+		student[1]="Jayapal";
+		student[2]="Ganesh";
+		student[3]="Mahesh";
+		student[4]="Karthik";
+		for(int i=0;i<=4;i++)
+		{
+			System.out.println(student[i]);
+		}
+		
+				
+				
+
+	}
+
+}
